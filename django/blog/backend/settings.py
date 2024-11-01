f2261bfef8c07796e141b576e6061e77ca2dea15
Stellan Ostlund <stellan.ostlund@gmail.com>
2024-11-01 16:10:06 +0100

    Clean up templates

--- django/blog/backend/settings.py
+++ django/blog/backend/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'backend.middleware.rewrite_response.ResponseRewriteMiddleware', 
 ]
 
 ROOT_URLCONF = "backend.urls"
