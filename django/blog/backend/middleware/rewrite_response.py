51cd19ec507e4f34a98693f44f0d415da1ab5d1b
Stellan Ostlund <stellan.ostlund@gmail.com>
2024-11-01 22:42:59 +0100

    OK

--- django/blog/backend/middleware/rewrite_response.py
+++ django/blog/backend/middleware/rewrite_response.py
@@ -3,19 +3,19 @@ from django.utils.deprecation import MiddlewareMixin
 class ResponseRewriteMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         # Check if the response is an HTML response
-        r = { 'OpenTA-category-not-selected' : 'hoverbg-green-400',\
+        r = { 'OpenTA-category-not-selected' : 'bg-green-200 hover:bg-green-400',\
             'OpenTA-category-selected' : 'bg-red-400',\
-            'OpenTA-body' : 'bg-bodycolor p-8',\
-            'OpenTA-comment-body' : 'p-0',\
+            'OpenTA-body' : 'bg-indigo-100 p-8 font-sans text-sm antialiased',\
+            'OpenTA-comment-body' : 'py-2',\
             'OpenTA-comment-entry' : 'p-0',\
-            'OpenTA-comment-list' : 'px-0' ,\
-            'OpenTA-hide-button' :  'bg-green-500',\
+            'OpenTA-comment-list' : 'p-0' ,\
+            'OpenTA-hide-button' :  'hover:bg-green-400 bg-green-200',\
             'OpenTA-leave-comment-form' : 'px-0',\
             'OpenTA-leave-comment-link' : 'bg-red-400',\
-            'OpenTA-navigation-bar' : 'bg-OpenTA_navigation',\
+            'OpenTA-navigation-bar' : 'bg-blue-100', \
             'OpenTA-post-entry' : 'p-0',\
-            'OpenTA-show-button' : 'bg-red-500',\
-            'OpenTA-submit-button' : 'btn btn-primary',\
+            'OpenTA-show-button' : 'hover:bg-red-400 bg-red-200',\
+            'OpenTA-submit-button' : 'hover:bg-blue-400 btn btn-primary',\
             'OpenTA-toggle' : 'sm:italic',}
         if 'text/html' in response['Content-Type']:
             for pat in r.keys() :
