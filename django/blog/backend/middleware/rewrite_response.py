554fb49dc66eabdc31f79a36cfcfb1e822b5a51e
Stellan Ostlund <stellan.ostlund@gmail.com>
2024-12-14 23:11:11 +0100

    OK

--- django/blog/backend/middleware/rewrite_response.py
+++ django/blog/backend/middleware/rewrite_response.py
@@ -7,16 +7,17 @@ class ResponseRewriteMiddleware(MiddlewareMixin):
             'OpenTA-category-selected' : 'inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white"',\
             'OpenTA-body' : 'bg-[#ffffff] p-4 font-sans text-sm antialiased',\
             'OpenTA-comment-body' : 'border border-gray-300 px-2 rounded-lg bg-red-400 shadow-md',\
-            'OpenTA-post-list' : 'h-full border border-gray-300 rounded-lg bg-white shadow-md',\
+            'OpenTA-post-list' : 'h-full border border-gray-300 rounded-lg bg-white shadow-md text-xs',\
             'OpenTA-comment-entry' : 'p-0',\
             'OpenTA-comment-list' : 'p-0 rounded-lg bg-white shadow-md ' ,\
             'OpenTA-hide-button' :  'hover:bg-blue-400 bg-blue-200',\
             'OpenTA-leave-comment-form' : 'px-0',\
             'OpenTA-leave-comment-link' : 'bg-blue-200',\
             'OpenTA-navigation-bar' : 'flex p-2 border-b bg-transparent bg-[#cccccc]', \
-            'OpenTA-post-entry' : 'border bg-blue-400 border-gray-300 p-4 rounded-lg bg-green-200  shadow-md',\
-            'OpenTA-post-body' : 'border  border-gray-300 p-4 rounded-lg bg-green-400  shadow-md',\
-            'OpenTA-post-last-modified' : 'font-light text-xs',\
+            'OpenTA-post-selected-entry' : 'border bg-blue-400 border-gray-300 p-2 rounded-lg bg-yellow-100  shadow-md',\
+            'OpenTA-post-entry' : 'border bg-blue-400 border-gray-300 p-2 rounded-lg bg-green-100  shadow-md',\
+            'OpenTA-post-body' : 'border  border-gray-300 p-4 rounded-lg bg-yellow-100  shadow-md',\
+            'OpenTA-post-last-modified' : 'font-light text-xs ',\
             'OpenTA-post-title' : 'italic bg-blue-400 ',\
             'OpenTA-show-button' : 'hover:bg-blue-400 bg-blue-200',\
             'OpenTA-submit-button' : 'hover:bg-blue-400 btn btn-primary',\
