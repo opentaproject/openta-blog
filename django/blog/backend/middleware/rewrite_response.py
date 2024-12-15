7b2d15c457c9bf69d1f67e38c9ba2c1b9e80fbfe
Stellan Ostlund <stellan.ostlund@gmail.com>
2024-12-15 22:04:59 +0100

    OK

--- django/blog/backend/middleware/rewrite_response.py
+++ django/blog/backend/middleware/rewrite_response.py
@@ -6,10 +6,10 @@ class ResponseRewriteMiddleware(MiddlewareMixin):
         r = { 'OpenTA-category-not-selected' : 'inline-block border border-white rounded hover:border-gray-200 hover:bg-gray-200 py-1 px-3',\
             'OpenTA-category-selected' : 'inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white"',\
             'OpenTA-body' : 'bg-[#ffffff] p-4 font-sans text-sm antialiased',\
-            'OpenTA-comment-body' : 'border-b border-gray-300 p-2 rounded-lg bg-red-100 shadow-md',\
+            'OpenTA-comment-body' : 'border-b border-gray-300 py-1 rounded-lg bg-white-100 shadow-md',\
             'OpenTA-post-list' : 'h-full border border-gray-300 rounded-lg shadow-md text-xs',\
-            'OpenTA-comment-entry' : 'border mt-2 p-2 rounded-lg bg-red-100',\
-            'OpenTA-comment-list' : 'p-0 rounded-lg bg-blue-100 shadow-md ' ,\
+            'OpenTA-comment-entry' : 'border-2 mt-2 p-2 border-gray-400 rounded-lg',\
+            'OpenTA-comment-list' : 'p-0 rounded-lg bg-white-100 shadow-md ' ,\
             'OpenTA-hide-button' :  'hover:bg-blue-400',\
             'OpenTA-leave-comment-form' : 'px-0',\
             'OpenTA-leave-comment-link' : '',\
@@ -18,7 +18,7 @@ class ResponseRewriteMiddleware(MiddlewareMixin):
             'OpenTA-post-entry' :          'border bg-white    border-gray-300 p-2 rounded-lg shadow-md',\
             'OpenTA-post-body' : 'border  border-gray-300 p-2 rounded-lg  bg-yellow-100 shadow-md',\
             'OpenTA-post-last-modified' : 'font-light text-xs ',\
-            'OpenTA-post-title' : 'bold',\
+            'OpenTA-post-title' : 'font-semibold',\
             'OpenTA-show-button' : 'hover:bg-blue-400 bg-blue-200',\
             'OpenTA-submit-button' : 'hover:bg-blue-400 btn btn-primary',\
             'OpenTA-toggle' : 'sm:italic',}
