bd1de13b75044508b49b0d6f4a3727230eaaa121
Stellan Ostlund <stellan.ostlund@gmail.com>
2024-12-15 16:23:29 +0100

    OK

--- django/blog/backend/middleware/rewrite_response.py
+++ django/blog/backend/middleware/rewrite_response.py
@@ -3,22 +3,22 @@ from django.utils.deprecation import MiddlewareMixin
 class ResponseRewriteMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         # Check if the response is an HTML response
-        r = { 'OpenTA-category-not-selected' : 'inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3',\
+        r = { 'OpenTA-category-not-selected' : 'inline-block border border-white rounded hover:border-gray-200 hover:bg-gray-200 py-1 px-3',\
             'OpenTA-category-selected' : 'inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white"',\
             'OpenTA-body' : 'bg-[#ffffff] p-4 font-sans text-sm antialiased',\
-            'OpenTA-comment-body' : 'border border-gray-300 px-2 rounded-lg bg-red-400 shadow-md',\
-            'OpenTA-post-list' : 'h-full border border-gray-300 rounded-lg bg-white shadow-md text-xs',\
-            'OpenTA-comment-entry' : 'p-0',\
-            'OpenTA-comment-list' : 'p-0 rounded-lg bg-white shadow-md ' ,\
-            'OpenTA-hide-button' :  'hover:bg-blue-400 bg-blue-200',\
+            'OpenTA-comment-body' : 'border-b border-gray-300 p-2 rounded-lg bg-red-100 shadow-md',\
+            'OpenTA-post-list' : 'h-full border border-gray-300 rounded-lg shadow-md text-xs',\
+            'OpenTA-comment-entry' : 'border mt-2 p-2 rounded-lg bg-red-100',\
+            'OpenTA-comment-list' : 'p-0 rounded-lg bg-blue-100 shadow-md ' ,\
+            'OpenTA-hide-button' :  'hover:bg-blue-400',\
             'OpenTA-leave-comment-form' : 'px-0',\
-            'OpenTA-leave-comment-link' : 'bg-blue-200',\
+            'OpenTA-leave-comment-link' : '',\
             'OpenTA-navigation-bar' : 'flex p-2 border-b bg-transparent bg-[#cccccc]', \
-            'OpenTA-post-selected-entry' : 'border bg-blue-400 border-gray-300 p-2 rounded-lg bg-yellow-100  shadow-md',\
-            'OpenTA-post-entry' : 'border bg-blue-400 border-gray-300 p-2 rounded-lg bg-green-100  shadow-md',\
-            'OpenTA-post-body' : 'border  border-gray-300 p-4 rounded-lg bg-yellow-100  shadow-md',\
+            'OpenTA-post-selected-entry' : 'border border-gray-300 p-2 rounded-lg bg-yellow-100  shadow-md',\
+            'OpenTA-post-entry' :          'border bg-white    border-gray-300 p-2 rounded-lg shadow-md',\
+            'OpenTA-post-body' : 'border  border-gray-300 p-2 rounded-lg  bg-yellow-100 shadow-md',\
             'OpenTA-post-last-modified' : 'font-light text-xs ',\
-            'OpenTA-post-title' : 'italic bg-blue-400 ',\
+            'OpenTA-post-title' : 'bold',\
             'OpenTA-show-button' : 'hover:bg-blue-400 bg-blue-200',\
             'OpenTA-submit-button' : 'hover:bg-blue-400 btn btn-primary',\
             'OpenTA-toggle' : 'sm:italic',}
