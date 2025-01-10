6fd3042e3066cc9c462b89bb62301e07075c22e4
Stellan Ostlund <stellan.ostlund@gmail.com>
2025-01-10 01:18:03 +0100

    OK; make truncation sensitive to width

--- django/blog/backend/middleware/rewrite_response.py
+++ django/blog/backend/middleware/rewrite_response.py
@@ -3,13 +3,14 @@ from django.utils.deprecation import MiddlewareMixin
 class ResponseRewriteMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         # Check if the response is an HTML response
-        r = { 'OpenTA-category-not-selected' : 'inline-block border border-white rounded hover:border-gray-200 hover:bg-gray-200 py-1 px-3',\
-            'OpenTA-category-selected' : 'inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white"',\
-            'OpenTA-body' : 'bg-[#ffffff] p-4 font-sans text-sm antialiased',\
-            'OpenTA-comment-body' : 'border-b border-gray-300 py-1 rounded-lg bg-white-100 shadow-md',\
-            'OpenTA-post-list' : 'h-full border border-gray-300 rounded-lg shadow-md text-xs',\
+        r = { 
+            'OpenTA-category-not-selected' : 'OpenTA-text-size inline-block border border-white rounded hover:border-gray-200 hover:bg-gray-200 py-0 px-2',\
+            'OpenTA-category-selected' : 'OpenTA-text-size  inline-block border border-blue-500 rounded py-0 px-2 bg-blue-500 text-white"',\
+            'OpenTA-body' : 'bg-[#ffffff] p-4 font-sans antialiased',\
+            'OpenTA-comment-body' : 'border-b border-gray-300 py-1 rounded-lg shadow-md',\
+            'OpenTA-post-list' : 'h-full border border-gray-300 rounded-lg shadow-md OpenTA-text-size',\
             'OpenTA-comment-entry' : 'border-2 mt-2 p-2 border-gray-400 rounded-lg',\
-            'OpenTA-comment-list' : 'p-0 rounded-lg bg-white-100 shadow-md bg-blue-100 ' ,\
+            'OpenTA-comment-list' : 'p-0 rounded-lg  shadow-md bg-blue-100 ' ,\
             'OpenTA-hide-button' :  'hover:bg-blue-400',\
             'OpenTA-leave-comment-form' : 'px-0',\
             'OpenTA-leave-comment-link' : '',\
@@ -18,14 +19,18 @@ class ResponseRewriteMiddleware(MiddlewareMixin):
             'OpenTA-post-rhs-entry' :      'bg-yellow-100 border border-gray-300 p-2 rounded-lg shadow-md',\
             'OpenTA-post-entry' :          'border border-gray-300 p-2 rounded-lg shadow-md',\
             'OpenTA-post-body' : '',\
-            'OpenTA-post-last-modified' : 'font-light text-xs ',\
-            'OpenTA-post-title' : 'font-semibold',\
+            'OpenTA-post-last-modified' : 'font-light OpenTA-text-size ',\
+            'OpenTA-post-title' : 'OpenTA-text-size font-semibold',\
             'OpenTA-show-button' : 'hover:bg-blue-400 bg-blue-200',\
             'OpenTA-submit-button' : 'hover:bg-blue-400 btn btn-primary',\
             'OpenTA-rhs-sidebyside' : 'w-3/4 align-top px-2',\
-            'OpenTA-lhs-sidebyside' : 'bg-white w-1/4 align-top px-2',\
+            'OpenTA-lhs-sidebyside' : 'OpenTA-background w-1/4 align-top px-2',\
             'OpenTA-table-sidebyside' : 'min-w-full border-collapse border border-blue-800',\
-            'OpenTA-toggle' : 'sm:italic',}
+            'OpenTA-new-button' : 'p-1 text-white bg-blue-400 font-medium rounded me-2 ',\
+            'OpenTA-toggle' : 'sm:italic',
+            'OpenTA-text-size' : 'text-xs',\
+            'OpenTA-background' : 'bg-white',\
+             }
     
         p = request.path
         try :
