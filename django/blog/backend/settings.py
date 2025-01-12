0a7254bfdaa1e1bba52a1e703b816f7f1a781aeb
Stellan Ostlund <stellan.ostlund@gmail.com>
2025-01-12 23:12:39 +0100

    Add more model fields and foreign keys; squash of 0ef59bd

--- django/blog/backend/settings.py
+++ django/blog/backend/settings.py
@@ -59,8 +59,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "crispy_forms",
-    "crispy_tailwind",
+    #"crispy_forms",
+    #"#crispy_tailwind",
 
 
 ]
@@ -343,6 +343,6 @@ PYLTI_CONFIG = {
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 LTI_EXTRA_PARAMETERS = ["custom_canvas_login_id"]
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-CRISPY_TEMPLATE_PACK = 'tailwind'
-CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+#CRISPY_TEMPLATE_PACK = 'tailwind'
+#CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 
