3de5e81c8e105e705f54541d5184c882a6517b28
Stellan Ostlund <stellan.ostlund@gmail.com>
2025-04-14 23:47:24 +0200

    OK

--- django/blog/sidecar_openai/admin.py
+++ django/blog/sidecar_openai/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import OpenAIFile  # Replace with your actual model
+from .models import OpenAIFile  , VectorStore # Replace with your actual model
 
 @admin.register(OpenAIFile)
 class OpenAIFileAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'original_file_name', 'file_id', 'date')
     readonly_fields = ('checksum','original_file_name','path','file_id')
+
+@admin.register(VectorStore)
+class VectorStoreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'vector_store_id', 'list_file_ids')  # Add your custom method here
+
+    def list_file_ids(self, obj):
+        return ", ".join(str(f.pk) for f in obj.files.all())
+
+    list_file_ids.short_description = "File IDs"
+    
--- django/blog/sidecar_openai/models.py
+++ django/blog/sidecar_openai/models.py
@@ -5,6 +5,8 @@ from django.conf import settings
 import hashlib
 import openai 
 from openai import OpenAI
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 
 import os
 client = openai.OpenAI(api_key=settings.AI_KEY)
@@ -23,7 +25,7 @@ def hashed_upload_to(instance, filename):
 
 class OpenAIFile(models.Model) :
     date = models.DateTimeField(auto_now=True)
-    checksum = models.CharField(primary_key=True, max_length=255,blank=True)
+    checksum = models.CharField(blank=True, max_length=255)
     original_file_name = models.CharField(max_length=255,blank=True)
     path = models.CharField(max_length=255,blank=True)
     file_id = models.CharField(max_length=255,blank=True)
@@ -32,10 +34,9 @@ class OpenAIFile(models.Model) :
 
 
     def save( self, *args, **kwargs ):
-        is_new = self._state.adding and not self.pk
-        self.original_file_name = f"{self.file}"
+        is_new = self._state.adding  and not self.pk
+        self.original_file_name = self.file.name
         super().save(*args, **kwargs)  # Save first, so file is processed
-        
         if is_new and self.file:
             data = self.file.read()
             print(f"FILE = {self.file}")
@@ -45,12 +46,47 @@ class OpenAIFile(models.Model) :
             self.file_id = uploaded_file.id
             super().save(*args, **kwargs) # Then update with true hashed path
 
-#class VectorStore( models.model ):
-#    vector_store_id = models.CharField(max_length=255,blank=True)
-#    files = ManyToManyFields( OpenTAFile )
-# 
-#
+class VectorStore( models.Model ):
+    vector_store_id = models.CharField(max_length=255,blank=True)
+    name =  models.CharField(max_length=255,blank=True)
+    files = models.ManyToManyField( OpenAIFile )
+
+    def save( self, *args, **kwargs ):
+        is_new = self._state.adding and not self.pk
+        super().save(*args,**kwargs)
+        if is_new :
+            print(f"{self.files.all() }")
+            vector_store = client.vector_stores.create(name=self.name)
+            self.vector_store_id = vector_store.id
+            super().save(*args,**kwargs)
+        #    #for f in self.files.all() :
+        #    file_ids = [ f.file_id for f in self.files.all()  ]
+        #    print(f'FILE_IDS = {file_ids}')
+
 #
 #    vector_store = client.vector_stores.create(name="Simple file")
 #    vector_store_id = vector_store.id
 
+@receiver(m2m_changed, sender=VectorStore.files.through)
+def handle_files_changed(sender, instance, action, **kwargs):
+    if action == "post_add":
+        if getattr(instance, '_updating_from_m2m', False):
+            return
+        instance._updating_from_m2m = True
+        print(f"Files updated for VectorStore: {instance.pk}")
+        print(f"FILES = {instance.files.all() }")
+        vector_store_id = instance.vector_store_id
+        print(f"VECTOR_STORE_ID = {vector_store_id}")
+        pks = [];
+        ids = [];
+        for f in instance.files.all() :
+            pks.append( f.pk )
+            ids.append( f.file_id );
+            print(f"F = {f.pk} {f.file_id} ")
+        print(f"PKS = {pks}")
+        print(f"IDS = {ids }")
+        instance.files.add( *pks )
+        instance.save()
+        del instance._updating_from_m2m
+
+        # Do something with instance.files.all()
