790419139ff8c0efc6b6e5f2211e4b630e0efab3
Stellan Ostlund <stellan.ostlund@gmail.com>
2025-04-18 02:28:19 +0200

    OK test

--- django/blog/sidecar_openai/tests.py
+++ django/blog/sidecar_openai/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from sidecar_openai.models import OpenAIFile, VectorStore, Assistant
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 
 # python manage.py test sidecar_openai.tests
 
@@ -17,6 +19,12 @@ class OpenAI(TestCase):
         url = reverse('admin:sidecar_openai_openaifile_changelist')  # use your app and model name
         response = self.client.get(url)
         print(f"RESPONSE = {response}")
+        url = reverse('admin:sidecar_openai_openaifile_add')  # use your app and model name
+        #response = self.client.get(url)
+        #print(f"RESPONSE2 = {response}")
+        test_file = SimpleUploadedFile( "test.txt", b"file_content_here", content_type="text/plain")
+        response3 = self.client.post( url ,  {'file': test_file}, follow=True)
+        print(f"RESPONS3 = {response3}")
         #self.assertEqual(response.status_code, 200)
         # Now POST data to create a new instance
         #response = self.client.post(url, {
@@ -24,9 +32,6 @@ class OpenAI(TestCase):
         #    'field2': 'value2',
         #    # ... include all required fields
         # })
-
-    def test1(self) :
-        #OpenAIFile.objects.create(file="testdir/txt3.txt")
         files = OpenAIFile.objects.all()
         print(f"FILES = {files}")
 
